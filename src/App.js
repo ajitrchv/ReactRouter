d6f478a049b6b6c15273bc80834000e6861f2d34
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-03-16 14:27:54 +0530

    end of this app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Products from "./pages/products.js";
 import Welcome from "./pages/welcome.js";
 import MainHeader from './components/MainHeader.js';
+import ProductDetail from "./pages/ProductDetail.js";
 
 function App() {
   return (
     <div>
       <MainHeader/>
       <main>
-      <Route path="/welcome">
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome'></Redirect>
+          </Route>
+        <Route path="/welcome">
         <Welcome></Welcome>
       </Route>
-      <Route path="/products">
+      <Route path="/products" exact>
         <Products></Products>
       </Route>
+      <Route path="/products/:productId">
+        <ProductDetail/>
+      </Route>
+        </Switch>
+      
       </main>
     </div>
-  );
-}
+  )
+};
 
 export default App;
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,6 +1,21 @@
-const Products = () =>
-{
-    return <h1>Products Page</h1>;
+import { Link } from "react-router-dom";
+const Products = () => {
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/mini">Mini</Link>
+        </li>
+        <li>
+          <Link to="/products/merc">Mercedes</Link>
+        </li>
+        <li>
+          <Link to="/products/toyota">Toyota</Link>
+        </li>
+      </ul>
+    </section>
+  )
 };
 
 export default Products;
